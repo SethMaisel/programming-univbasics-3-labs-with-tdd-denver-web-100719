bab68e04746cdef5d8bcf5ea44349779276caa88
Seth Maisel <seth.maisel@gmail.com>
2019-09-28 23:30:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,11 @@ second_number = 12
 sum = first_number + second_number;
 puts sum;
 
+difference = first_number - second_number;
+puts difference;
+
+product = first_number * second_number;
+puts product
+
+quotient = first_number / second_number;
+puts quotient
